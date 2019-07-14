92e7176fe0b419d8fe7eb8e6f3f848b5e8cb817b
Ian Bearman <ianb@microsoft.com>
2019-07-14 15:59:28 -0700

    adding more ttt game stuff

--- src/pen-island-winforms/pen-island-core/TttBoard.cs
+++ src/pen-island-winforms/pen-island-core/TttBoard.cs
@@ -12,6 +12,8 @@ namespace PenIsland
 {
     public partial class TttBoard : UserControl
     {
+        internal TttGame TttGame { get; private set; }
+
         public TttBoard()
         {
             InitializeComponent();
@@ -19,25 +21,57 @@ namespace PenIsland
 
         public void NewGame()
         {
-           // DotsGame = new DotsGame(DotsGameSettings.PlayerCount, DotsGameSettings.BoardType, DotsGameSettings.BoardWidth, DotsGameSettings.BoardHeight);
+            // TttGame = new TttGame(TttGameSettings.PlayerCount, TttGameSettings.BoardWidth, TttGameSettings.BoardHeight);
+            TttGame = new TttGame(2, 3, 3);
 
             ClientSize = GetPreferedWindowSize();
             Refresh();
 
-//            RunComputerPlayers();
+            RunComputerPlayers();
         }
+
+        static readonly int PreferedSpacer = 50;
+        static readonly int PreferedBorder = PreferedSpacer / 2;
+
+
+        // default size:
+        //   <-- 20 pixels --> <-- 40 pixels --> | <-- 40 pixels --> | <-- 40 pixels --> <-- 25 pixels ->
+        //   spacing is same horizontal and vertical
 
         public Size GetPreferedWindowSize()
         {
             //return new Size(DotsGame.Width * PreferedSpacer + PreferedDotSize, DotsGame.Height * PreferedSpacer + PreferedDotSize);
-            return new Size(100, 100);
+            return new Size((PreferedSpacer * TttGame.Width) + PreferedBorder * 2, (PreferedSpacer * TttGame.Height) + PreferedBorder * 2);
         }
 
         private void TTTBoard_Paint(object sender, PaintEventArgs e)
         {
             var g = e.Graphics;
 
-            g.DrawString("I'm a tic-tac-toe board", SystemFonts.DefaultFont, Brushes.BlueViolet, new Point(0, 0));
+            g.FillRectangle(Brushes.White, this.ClientRectangle);
+
+            if (TttGame == null)
+                return;
+
+            // draw the vertical lines
+            int x = PreferedBorder + PreferedSpacer;
+            for (int i = 0; i < TttGame.Width - 1; ++i, x += PreferedSpacer)
+            {
+                g.DrawLine(Pens.Black, new Point(x, PreferedBorder), new Point(x, PreferedBorder + (PreferedSpacer * TttGame.Height)));
+            }
+
+            // draw the horizontal lines
+            int y = PreferedBorder + PreferedSpacer;
+            for (int j = 0; j < TttGame.Height - 1; ++j, y += PreferedSpacer)
+            {
+                g.DrawLine(Pens.Black, new Point(PreferedBorder, y), new Point(PreferedBorder + (PreferedSpacer * TttGame.Width), y));
+            }
+
+        }
+
+        void RunComputerPlayers()
+        {
+
         }
     }
 }
---
+++ src/pen-island-winforms/pen-island-core/TttGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenIsland
+{
+    class TttGame
+    {
+        public int PlayerCount { get; private set; }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public TttGame(int playerCount, int boardWidth, int boardHeight)
+        {
+            PlayerCount = playerCount;
+
+            Width = boardWidth;
+            Height = boardHeight;
+
+        }
+    }
+}
