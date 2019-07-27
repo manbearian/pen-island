4bead9d0e062590fa5460db8f4ab3cda4ba547a3
Ian Bearman <ianb@microsoft.com>
2019-07-26 19:42:41 -0700

    add minimax algorithm for TttAutoPlayer; refactor TttGame for easy access

--- src/pen-island-winforms/pen-island-core/AutoPlayer.cs
+++ src/pen-island-winforms/pen-island-core/AutoPlayer.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PenIsland
 {
@@ -250,24 +247,114 @@ namespace PenIsland
     class GameTreeNode
     {
         public GameTreeNode(TttGameState state) {
-            this.state = state;
+            State = state;
         }
-        TttGameState state;
+        public TttGameState State;
         public List<GameTreeNode> Children = new List<GameTreeNode>();
+        public int Score = 0;
     }
 
     class TttAutoPlayer
     {
+        public static MoveInfo StateDiff(TttGame game, TttGameState before, TttGameState after)
+        {
+            MoveInfo? move = null;
+            for (int i = 0; i < game.Width; ++i)
+            {
+                for (int j = 0; j < game.Height; ++j)
+                {
+                    if (after[i,j] != before[i,j])
+                    {
+                        // ensure the only delta is a single new move
+                        System.Diagnostics.Debug.Assert(after[i, j] != Player.Invalid);
+                        System.Diagnostics.Debug.Assert(before[i, j] == Player.Invalid);
+
+                        // this function is only called to figure out what move the current player played
+                        System.Diagnostics.Debug.Assert(after[i, j] == game.CurrentPlayer);
+
+#if DEBUG
+                        System.Diagnostics.Debug.Assert(move == null);
+                        move = new MoveInfo(i, j);
+#else
+                        break;
+#endif
+                    }
+                }
+            }
+
+            System.Diagnostics.Debug.Assert(move != null);
+            return move.Value;
+        }
 
         public static void MakeMove(TttGame game)
         {
             GameTree tree = new GameTree(game);
-            ExpandOneLevel(tree.head);
+        
+            Expand(tree);
+
+            MoveInfo? bestMove = null;
+            int bestScore = int.MinValue;
+            foreach (var child in tree.head.Children)
+            {
+                var move = StateDiff(game, tree.head.State, child.State);
+
+                if (child.Score > bestScore)
+                {
+                    bestMove = move;
+                    bestScore = child.Score;
+                }
+            }
+
+            game.RecordMove(bestMove.Value);
         }
 
-        static void ExpandOneLevel(GameTreeNode node)
+        static void Expand(GameTree tree)
         {
-            
+            ExpandOneLevel(tree, tree.head, 0);
+        }
+
+        static void ExpandOneLevel(GameTree tree, GameTreeNode node, int depth)
+        {
+            var playerAtDepth = (tree.game.CurrentPlayer + depth) % tree.game.PlayerCount;
+
+            var winner = tree.game.CheckWinner(node.State);
+            if (winner != null)
+            {
+                if (winner == tree.game.CurrentPlayer)
+                    node.Score = 10 - depth;
+                else if (winner == Player.Invalid)
+                    node.Score = 0;
+                else
+                    node.Score = depth - 10;
+                return;
+            }
+
+            for (int i = 0; i < tree.game.Width; ++i)
+            {
+                for (int j = 0; j < tree.game.Height; ++j)
+                {
+                    if (node.State[i, j] == Player.Invalid)
+                    {
+                        var nextState = node.State.Clone();
+                        nextState[i, j] = playerAtDepth;
+                        node.Children.Add(new GameTreeNode(nextState));
+                    }
+                }
+            }
+
+            System.Diagnostics.Debug.Assert(node.Children.Count > 0, "there should be one move remaining or we wouldn't have recursed");
+
+            var depthPlayerIsMe = playerAtDepth == tree.game.CurrentPlayer;
+            int score = depthPlayerIsMe ? int.MinValue : int.MaxValue;
+            foreach (var child in node.Children)
+            {
+                ExpandOneLevel(tree, child, depth + 1);
+                if (depthPlayerIsMe)
+                    score = Math.Max(score, child.Score);
+                else
+                    score = Math.Min(score, child.Score);
+            }
+            node.Score = score;
         }
     }
 
